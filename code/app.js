9eadc9ddad7bfb2101034ef98d8ff619a23f721f
Carl Cauchi <carl.cauchi@keyrock.eu>
2024-05-21 20:14:04 +0200

    updated app

--- code/app.js
+++ code/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const Web3 = require('web3');
 
 const app = express();
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+// Connect to an Ethereum node (e.g., Infura)
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/9cb28c4a374248b98e156106f2dde570'));
 
+// Define a route to get the latest block number
 app.get('/', async (req, res) => {
-  const blockNumber = await web3.eth.getBlockNumber();
-  res.send(`Current Ethereum Block Number: ${blockNumber}`);
+  try {
+    const latestBlockNumber = await web3.eth.getBlockNumber();
+    res.send(`The latest block number is ${latestBlockNumber}`);
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
